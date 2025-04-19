b987ebdbcb4949ff6719d17f561ab491694d0a1e
Juan Vidal <juanmvg2003@gmail.com>
2025-04-19 14:37:51 -0500

    add SEO to the landing page

--- src/views/landing/Index.jsx
+++ src/views/landing/Index.jsx
@@ -1,3 +1,4 @@
+import { HelmetProvider, Helmet } from "react-helmet-async";
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import About from "./components/About";
@@ -6,19 +7,33 @@ import Sermones from "./components/Sermones/Sermones";
 import Reuniones from "./components/Reuniones/Index";
 
 const Index = () => {
-  document.title = "Iglesia Panamericana Centro de Fe y Esperanza Popayán";
-
   return (
-    <div className="font-body">
-      <NavBar/>
-      <main className="overflow-x-hidden">
-        <Hero/>
-        <About/>
-        <Sermones/>
-        <Reuniones/>
-        <Footer/>
-      </main> 
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <title>Iglesia Panamericana Centro de Fe y Esperanza Popayán</title>
+        <meta name="description" content="Únete a la Iglesia Cristiana Panamericana Fe y Esperanza en Popayán. Ven y experimenta el amor de Dios, enseñanzas bíblicas y una comunidad que transforma vidas." />
+        <meta name="robots" content="index, follow" />
+        <meta name="language" content="es" />
+        <meta property="og:title" content="Iglesia Cristiana Fe y Esperanza - Popayán" />
+        <meta property="og:description" content="Te invitamos a la Iglesia Cristiana Panamericana Fe y Esperanza en Popayán: un lugar de fe, esperanza y amor en comunidad. ¡Ven y forma parte de nuestra familia espiritual!" />
+        <meta property="og:image" content="https://cfepopayan.org/assets/imagen1.jpg" />
+        <meta property="og:url" content="https://cfepopayan.org" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="es_CO" />
+        <link rel="canonical" href="https://cfepopayan.org" />
+      </Helmet>
+      <div className="font-body">
+        <NavBar/>
+        <main className="overflow-x-hidden">
+          <Hero/>
+          <About/>
+          <Sermones/>
+          <Reuniones/>
+          <Footer/>
+        </main> 
+      </div>
+    </HelmetProvider>
+
   );
 };
 
